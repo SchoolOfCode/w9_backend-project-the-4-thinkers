358f8e073184b10c47dc4df3650d2bfc42d9512f
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-06-29 14:26:10 +0100

    Supertest route tests completed

--- routes/comments.test.js
+++ routes/comments.test.js
@@ -1,12 +1,7 @@
 import { test, expect, describe } from "@jest/globals";
 import request from "supertest";
 import app from "../app.js";
-import commentsRouter from "./comments";
-import {
-    getAllComments,
-    createComment,
-    deleteComment,
-} from "../models/comments";
+
 
 // TEST the get all comments function
 
@@ -48,38 +43,59 @@ describe("GET all comments", function () {
 //     await pool.end();
 // });
 
-// TASK 3.6.4 Add a new user to database
+// TEST the post comments function
 
-// describe("POST new user", function () {
-//   test("check whether new user is added to DB", async function createUser() {
-//     const response = await request(app).get("/users");
-//     expect(response.statusCode).toBe(200);
-//     console.log(response.statusCode);
-//     expect(response.body).toMatchObject({
-//       success: true,
-//       payload: expect.any(Array),
-//     });
-//     // console.log(response.body);
+describe("POST new comments", function () {
+    test("Check whether new comment is added to database", async function createComment() {
+        await new Promise((resolve) => setTimeout(() => resolve(), 2000));
+        const response = await request(app)
+            .post("/page/1")
+            .set("Accept", "application/json")
+            .send({comment_text: "test comment"})
+            expect(response.body).toMatchObject({
+                success: true,
+                message: expect.any(String),
+                payload: expect.any(Object),
+            });
+    });
+    test("Check that the comment_text has a string", async function createComment (){
+        await new Promise((resolve) => setTimeout(() => resolve(), 2000));
+        const testUserId = 27;
+        const testCommentText = "This is a comment.";
+        const response = await request(app)
+            .post("/page/1")
+            .set("Accept", "application/json")
+            .send({
+                comment_text: testCommentText,
+                user_id: testUserId
+            })
+        expect(response.body.payload.rows[0]).toMatchObject({
+            comments_id: expect.any(Number),
+            user_id: testUserId,
+            comment_text: testCommentText,
+            page_id: expect.any(Number)
+        });
+    });  
+});
 
-//     expect(response.body.payload).toEqual(
-//       expect.arrayContaining([
-//         expect.objectContaining({
-//           id: expect.any(Number),
-//           username: "Kal",
-//         }),
-//       ])
-//     );
-//     console.log(response.body.payload);
-//   });
-// });
+// TEST the delete comments function
 
-// // TASK 3.6.5 DELETE user from database
-
-// describe("DELETE user with specific ID", function () {
-//   test("check whether user with specific ID is deleted", async function deleteUserById() {
-//     const response = await request(app).get("/users/204");
-//     expect(response.statusCode).toBe(404);
-//     console.log(response.statusCode);
-//     console.log(response.text);
-//   });
-// });
+describe("DELETE comment with specific ID", function () {
+    test("Check whether a comment is deleted at all", async function () {
+        await new Promise((resolve) => setTimeout(() => resolve(), 2000));
+        const response = await request(app)
+            .delete("/page/1")
+            .set("Accept", "application/json")
+            .send({commentId:20});
+        expect(response.statusCode).toBe(200);
+        expect(response.body.payload.command).toBe("DELETE");
+    });
+    test("Check whether an empty array is returned when deleting", async function () {
+        await new Promise((resolve) => setTimeout(() => resolve(), 2000));
+        const response = await request(app)
+            .delete("/page/1")
+            .set("Accept", "application/json")
+            .send({commentId:20});
+        expect(response.body.payload.rows).toStrictEqual([]);
+    });
+});
